38a1efed823e144e783ce656d673f4b3c10d3cfb
kirb <44183742+asatacable@users.noreply.github.com>
2021-12-06 22:33:22 -0330

    funny ip change

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html")
+    
+    return render_template("home.html", clientip=request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
