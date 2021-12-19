dd51d88c398de80e17fbf457c10d7620caca8c2b
kirb <44183742+asatacable@users.noreply.github.com>
2021-12-19 14:27:36 -0330

    commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLalchmey
+
+
+db = SQLalchmey
+DB_Name = "database.db"
+
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_Name}'
+    db.init_app(app) 
 
     from .views import views
     from .auth import auth
---
+++ website/models.py
@@ -0,0 +1,8 @@
+from . import db 
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.string(150), unique=True)
+    password = db.Column(db.string(150))
+    username =db.Column(db.string(150), unique=True)
