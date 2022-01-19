5b4bd630f296a9e337ba2d9fb1d2991735b69ed2
kirb <44183742+asatacable@users.noreply.github.com>
2022-01-19 20:07:52 -0330

    changed all the shit

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,39 @@
-import re
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    
-    return render_template("login.html", bool=True)
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('username does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout function<p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -20,15 +43,24 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Email not valid', category='error')
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('username already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(username) < 2:
-            flash('username must be greater than 1 character', category='error')
+            flash('Username must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords don\' match', category='error')
-        elif len(password1) <7:
-            flash('Password must be greater than 6 character', category='error')
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            flash('Account created', category='success')
-            #add user
-    return render_template("sign_up.html")
+            new_user = User(email=email, username=username, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
--- website/views.py
+++ website/views.py
@@ -1,8 +1,81 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, send_file, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Post
+from . import db
+import json
+import datetime
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+import random
+
+from werkzeug.utils import redirect
+
+def get_bottom_string():
+    ascii_list = ['vdsdhh', 'hasj', 'xcvhr', 'hjjghf', 'nhas', 'djfn', 'gdxj', 'f', ' god ', 'ja', ' fd', ' gaihjs']
+    result_str = ''.join(random.choice(ascii_list) for i in range(5))
+    return result_str
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    
-    return render_template("home.html")
+    return render_template("home.html", clientip=request.remote_addr)
+
+@views.route('/forum', methods=['GET', 'POST'])
+@login_required
+def Forum():
+    if request.method == 'POST':
+        post = request.form.get('post')
+
+        if len(post) < 1:
+            flash('post is too short!', category='error')
+        else:
+            new_post = Post(data=post, user_id=current_user.id)
+            db.session.add(new_post)
+            db.session.commit()
+            flash('post added!', category='success')
+
+    return render_template("forum.html", user=current_user)
+
+@views.route('/delete-post', methods=['POST'])
+def delete_post():
+    post = json.loads(request.data)
+    postId = post['postId']
+    post = post.query.get(postId)
+    if post:
+        if post.user_id == current_user.id:
+            db.session.delete(post)
+            db.session.commit()
+
+    return jsonify({})
+
+@views.route('/rcg/')
+def rcg():
+
+    font = ImageFont.truetype(font=r'/home/ubuntu/website/website/static/Whitney-Book.otf', size=22)
+    img = Image.open(r"/home/ubuntu/website/website/static/cuz_temp.png")
+    I1 = ImageDraw.Draw(img)
+    random_text = get_bottom_string()
+    I1.text(xy=(78, 34), text=random_text, fill="White", font=font)
+    byte_io = BytesIO()
+    img.save(byte_io, 'PNG')
+    byte_io.seek(0)
+
+    return send_file(byte_io, mimetype='image/png')
+
+@views.route('/toajjzwtajzwotatn/')
+def troll():
+
+    fun = random.randint(1, 5)
+
+    if fun == 5:
+        return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    else:
+        return send_file(r"/home/ubuntu/website/website/static/qr.png")
+
+@views.route('/crsitamasagjk/')
+def xmas():
+    if datetime.datetime.now() > datetime.datetime(2022, 12, 25):
+        return '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/sUNKxsVONug?controls=0&amp&start=16&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    else:
+        return render_template("xmas.html", )
