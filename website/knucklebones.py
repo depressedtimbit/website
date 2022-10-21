8467ec9591277c2b950e27ea194c7f819cb6244e
depressedtimbit <84045706+depressedtimbit@users.noreply.github.com>
2022-10-21 14:15:19 -0230

    some updates

--- website/knucklebones.py
+++ website/knucklebones.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask import Blueprint, flash, render_template, request, session, redirect, url_for
 from flask_socketio import emit
 from . import socketio
 
@@ -11,18 +11,19 @@ def lobby():
     if request.method == 'POST':
         session['name'] = request.form.get("username")
         session['room'] = request.form.get("room-id")
+        return redirect(url_for("bones.game"))
     return render_template("knucklebones_lobby.html.j2")
 
 @bones.route('/game')
 def game():
-    username = session.get('username', '')
-    room = session.get('room-id', '')
+    username = session.get('name', '')
+    room = session.get('room', '')
     if username == '' or room == '':
+        flash("no room", category="error")
         return redirect(url_for('.lobby'))
-    return render_template('knucklebones.html.j2',username=username, room=room)
+    return render_template('knucklebones.html',username=username, room=room)
 
-@socketio.on('hello')
-def handle_message(data):
-    print('received message: ' + data["data"])
-    emit('hi', {'data' : "I heard!"})
+@socketio.on('joined')
+def handle_message(Data):
+    print(Data)
     
