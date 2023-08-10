679fe7d8703fa4eb7f4037bbdb67d120514d1597
depressedtimbit <84045706+depressedtimbit@users.noreply.github.com>
2023-08-09 23:20:37 -0230

    commit

--- website/knucklebones.py
+++ website/knucklebones.py
@@ -1,29 +1,104 @@
 from flask import Blueprint, flash, render_template, request, session, redirect, url_for
-from flask_socketio import emit
+from flask_socketio import emit, join_room
+from random import randint
 from . import socketio
 
 bones = Blueprint('bones', __name__)
 
+class Gamesmanager():
+    def __init__(self) -> None:
+        self.activeGames = {}
+    def newgame(self, gameid):
+        if not gameid in self.activeGames:
+            self.activeGames[gameid]=Game(gameid)
+        return self.activeGames[gameid]
+    def removegame(self, gameid):
+        self.activeGames.pop(gameid)
+class player():
+    def __init__(self, name) -> None:
+        self.name = name
+        self.board = []
+        for i in range(3):
+            row = [0, 0, 0]
+            self.board.append(row)
+        print(self.board)
+            
+class Game():
+    def __init__(self, gameid) -> None:
+        self.gameid = gameid
+        self.players = []
+        self.turn = 0
+        self.lastroll = 0
+    def playerjoined(self, newplayername):
+        self.players.append(player(newplayername))
+    def update_board(self):
+        player1 = self.players[0]
+        player2 = self.players[1]
+        return [player1.board, player2.board]
+    def player_selection(self, playerselection):
+        player = self.players[self.turn]
+        oplayer = self.players[-self.turn+1]
+        for i, val in enumerate(player.board[playerselection]):
+            #print(player.board[playerselection])
+            if val == 0:
+                    player.board[playerselection][i] = self.lastroll
+                    for oi, oval in enumerate(oplayer.board[playerselection]):
+                        print(f'{oval}:{self.lastroll}')
+                        if oval == self.lastroll:
+                            oplayer.board[playerselection][oi] = 0
+                    return True
+                
+        
 
+gamemanager = Gamesmanager()
 
-@bones.route('/', methods=['GET', 'POST'])
+@bones.route('/')
 def lobby():
-    if request.method == 'POST':
-        session['name'] = request.form.get("username")
-        session['room'] = request.form.get("room-id")
-        return redirect(url_for("bones.game"))
     return render_template("knucklebones_lobby.html.j2")
 
-@bones.route('/game')
-def game():
-    username = session.get('name', '')
-    room = session.get('room', '')
-    if username == '' or room == '':
-        flash("no room", category="error")
-        return redirect(url_for('.lobby'))
-    return render_template('knucklebones.html',username=username, room=room)
+@bones.route('/fullscreen')
+def fullscreen():
+    return render_template("knucklebones_black.html.j2")
 
-@socketio.on('joined')
-def handle_message(Data):
-    print(Data)
-    
+
+@socketio.on('join')
+def handle_message(data):
+    room = data['Room']
+    Game = gamemanager.newgame(room)
+    Game.playerjoined(data["Player"])
+    playerlist = []
+    for player in Game.players:
+        playerlist.append(player.name)
+    join_room(room)
+    #send({'PlayerJoined': playerlist}, to=data["Room"])
+    emit("PlayerJoined", playerlist, room=room)
+    return len(playerlist) -1
+ 
+@socketio.on('startLobby')
+def handle_message(data):
+    room = data['Room']
+    Game = gamemanager.activeGames[room]
+    roll = randint(1, 6)
+    Game.lastroll = roll
+    emit('newturn', [Game.turn, roll], to=room)
+    emit('update-board', Game.update_board(), to=room)
+
+@socketio.on('progressTurn')
+def handle_message(data):
+    room = data['Room']
+    player = data['Player']
+    choice = data['Choice']
+    game:Game = gamemanager.activeGames[room]
+    if game.turn == player:
+        valid_choice = game.player_selection(choice)
+        print(valid_choice)
+        if valid_choice:
+            roll = randint(1, 6)
+            game.lastroll = roll
+            if game.turn == 1:
+                game.turn = 0
+            else: game.turn = 1
+            emit('newturn', [game.turn, roll], to=room)
+            emit('update-board', game.update_board(), to=room)
+
+
--- website/static/scripts/knucklebones.js
+++ website/static/scripts/knucklebones.js
@@ -30,75 +30,119 @@ function init() {
             placeholder="room-id"
           />
         </div>
-        <button type="submit" action="joinLobby()">Join</button>  
+        <button type="submit" onclick="joinLobby();">Join</button>  
     `;
     }
     
     function spawnGameMenu() {
+
       container.innerHTML = `
       <div class="game-flex-container">
-      <div class="knc-player-title grid-container">
+      <div class="knc-player-title ">
               <h3 align="center" id="PlayerName1">Waiting for Player to Join</h3>
-              <h4 align="center" id="PlayerScore1">0</h4>
-              <div class="grid-item player1" id="diceroller">X</div>
+              <h4 class="block" align="center" id="0ps">0</h4>
+              <div class="grid-item block player1" id="diceroller0">X</div>
       </div>
-              <div class="grids-container">
-                      <div class="grid-container">
-                              <div class="grid-item player1 col1" id=3>3</div>
-                              <div class="grid-item player1 col2" id=3>3</div>
-                              <div class="grid-item player1 col3" id=3>3</div>
-                              <div class="grid-item player1 col1" id=2>2</div>
-                              <div class="grid-item player1 col2" id=2>2</div>
-                              <div class="grid-item player1 col3" id=2>2</div>
-                              <div class="grid-item player1 col1" id=1>1</div>
-                              <div class="grid-item player1 col2" id=1>1</div>
-                              <div class="grid-item player1 col3" id=1>1</div>
-                      </div>
-              
-                      <div class="grid-container">
-                              <div class="grid-item player2 col1" id=1>1</div>
-                              <div class="grid-item player2 col2" id=1>1</div>
-                              <div class="grid-item player2 col3" id=1>1</div>
-                              <div class="grid-item player2 col1" id=2>2</div>
-                              <div class="grid-item player2 col2" id=2>2</div>
-                              <div class="grid-item player2 col3" id=2>2</div>
-                              <div class="grid-item player2 col1" id=3>3</div>
-                              <div class="grid-item player2 col2" id=3>3</div>
-                              <div class="grid-item player2 col3" id=3>3</div>
-                      </div>
-              </div>
+        <div class="knc-board">
+        <div class="grid-container">
+                <div class="knc-col" onclick="progressTurn(0);">
+                  <div class="grid-item player1 block col1" id=002>002</div>
+                  <div class="grid-item player1 block col1" id=001>001</div>
+                  <div class="grid-item player1 block col1" id=000>000</div>
+                  <div class="grid-item player1 block col1 score" id=00s>00s</div>
+                  <div class="grid-item spacer block col1"></div>
+                  <div class="grid-item player2 block col1 score" id=10s>10s</div>
+                  <div class="grid-item player2 block col1" id=100>100</div>
+                  <div class="grid-item player2 block col1" id=101>101</div>
+                  <div class="grid-item player2 block col1" id=102>102</div>
+                  </div>
+                  <div class="knc-col" onclick="progressTurn(1);">
+                  <div class="grid-item player1 block col2" id=012>012</div>
+                  <div class="grid-item player1 block col2" id=011>011</div>
+                  <div class="grid-item player1 block col2" id=010>010</div>
+                  <div class="grid-item player1 block col2 score" id=01s>01s</div>
+                  <div class="grid-item spacer block col2"></div>
+                  <div class="grid-item player2 block col2 score" id=11s>11s</div>
+                  <div class="grid-item player2 block col2" id=110>110</div>
+                  <div class="grid-item player2 block col2" id=111>111</div>
+                  <div class="grid-item player2 block col2" id=112>112</div>
+                  </div>
+                  <div class="knc-col" onclick="progressTurn(2);">
+                  <div class="grid-item player1 block col3" id=022>022</div>
+                  <div class="grid-item player1 block col3" id=021>021</div>
+                  <div class="grid-item player1 block col3" id=020>020</div>
+                  <div class="grid-item player1 block col3 score" id=02s>02s</div>
+                  <div class="grid-item spacer block col3"></div>
+                  <div class="grid-item player2 block col3 score" id=12s>12s</div>
+                  <div class="grid-item player2 block col3" id=120>120</div>
+                  <div class="grid-item player2 block col3" id=121>121</div>
+                  <div class="grid-item player2 block col3" id=122>122</div>
+                  </div>
+            </div>
+      </div>
       <div  class="knc-player-title">
-              <div class="grid-item player2" id="diceroller">X</div>
-              <h3 align="center" id="PlayerName2">Waiting for Player to Join</h3>
-              <h4 align="center" id="PlayerScore2">0</h4>
+      <div class="grid-item block " id="diceroller1">X</div>
+      <h3 align="center" id="PlayerName2">Waiting for Player to Join</h3>
+      <h4 class="block" align="center" id="1ps">0</h4>
       </div>
+    </div>
 </div>
     `;
     }
 
     var socket = io();
     function joinLobby() {
-      var player = document.getElementById("username").value; 
-      var room = document.getElementById("room-id").value;
+      player = document.getElementById("username").value; 
+      room = document.getElementById("room-id").value;
       spawnGameMenu()
       socket.emit('join', {Player: player, Room: room}, (response) => {
+
         console.log(response)
-        player = response[0]
+
+        player = response
       });
       }
-      socket.on('PlayerJoined', (response) => {
+    socket
+    socket.on('PlayerJoined', (response) => {
         console.log(response)
+        document.getElementById("PlayerName1").innerText = response[0]
         if (response.length > 1) {
           document.getElementById("PlayerName2").innerText = response[1]
+          if (player == 0) {startLobby();}
         }
-        document.getElementById("PlayerName1").innerText = response[0]
       })
-      socket.on("newturn", (response) => {
-        console.log(response)
+    socket.on("newturn", (response) => {
         activeplayer = response[0]
-        document.getElementsByClassName("player"+activeplayer).getElementById("diceroller").innerText=response[2]
+        document.getElementById('diceroller'+(activeplayer)).innerText=response[1]
+        document.getElementById('diceroller'+(-activeplayer+1)).innerText='X'
       })
-
+      
       socket.on("update-board", (response) =>{
         console.log(response)
+        for (board = 0; board < response.length; board++) {
+          var pscore = 0
+          for (row = 0; row < response[board].length; row++) {
+            document.getElementById(''+board+row+0).innerText=response[board][row][0]
+            document.getElementById(''+board+row+1).innerText=response[board][row][1]
+            document.getElementById(''+board+row+2).innerText=response[board][row][2]
+            list=[response[board][row][0], response[board][row][1], response[board][row][2]]
+            var hist = {};
+            var score = 0
+            list.map( function (a) { if (a in hist) hist[a] ++; else hist[a] = 1; } );
+            console.log(hist)
+            for (var key in hist) {
+              console.log((key*hist[key])*hist[key]);
+              score += (key*hist[key])*hist[key]
+            }
+            document.getElementById(''+board+row+'s').innerText=score
+            pscore += score
+          }
+        document.getElementById(''+board+'ps').innerText = pscore
+        } 
       })
+    function startLobby() {
+      socket.emit('startLobby', {Player: player, Room: room});
+    }
+    function progressTurn(choice) {
+      socket.emit('progressTurn', {Player: player, Room: room, Choice: choice});
+    }
